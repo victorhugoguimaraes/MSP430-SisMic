223e77b44acc0c4850720450fabb8177958190f4
Victor Guimarães <victorguinascimento@gmail.com>
2026-03-25 11:25:57 -0300

    Atualizar o m1ex1.asm

--- MODULO 1/m1ex01/m1ex1.asm
+++ MODULO 1/m1ex01/m1ex1.asm
@@ -11,7 +11,7 @@ main:
   ;mov.b #0xFE, @R4  erro que aparece -> [E0004] Illegal destination operand, devido ao @R4
   mov.b #0xCA, 1(R4)
   mov.w #0x1234, 2(R4)
-  mov.w #0x5678, 2(R4)
+  mov.w #0x5678, 4(R4)
 
   mov.w #0xABCD, 5(R4)
   jmp   $
